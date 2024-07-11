6f294421d1199cfbf249109ceb3f42f88d821b42
Jett <jettjaniak@gmail.com>
2024-07-11 10:44:30 +0100

    setup.py: delphi -> teren, remove static dir

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="delphi",
+    name="teren",
     packages=find_packages(where="."),
     package_dir={"": "."},
-    package_data={
-        "delphi": ["test_configs/**/*"],
-    },
     include_package_data=True,
 )
