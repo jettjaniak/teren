f68836e6f4b2bbadbde42287bca86ed86db081c3
Jett Janiak <jettjaniak@gmail.com>
2024-07-12 13:49:28 +0000

    perturb. revamp & notebook

--- teren/perturbations.py
+++ teren/perturbations.py
@@ -1,39 +1,53 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import final
 
 import torch
 from jaxtyping import Float
 from sae_lens import SAE
 
 
+@dataclass(kw_only=True)
 class Perturbation(ABC):
-    @abstractmethod
-    def __new__(
-        cls, resid_acts: Float[torch.Tensor, "... n_ctx d_model"], **kwargs
+    @final
+    def __call__(
+        self, resid_acts: Float[torch.Tensor, "... n_ctx d_model"]
     ) -> Float[torch.Tensor, "... n_ctx d_model"]:
+        """Ensures that all generate method has correct signature with beartype"""
+        return self.generate(resid_acts)
+
+    @abstractmethod
+    def generate(self, resid_acts: Float[torch.Tensor, "... n_ctx d_model"]):
         raise NotImplementedError
 
 
+@dataclass
 class NaiveRandomPerturbation(Perturbation):
-    def __new__(
-        cls, resid_acts: Float[torch.Tensor, "... n_ctx d_model"], **kwargs
-    ) -> Float[torch.Tensor, "... n_ctx d_model"]:
+    """Isotropic random"""
+
+    def generate(self, resid_acts):
         return torch.randn(resid_acts.shape)
 
 
+naive_random_perturbation = NaiveRandomPerturbation()
+
+
+@dataclass
 class TowardSAEReconPerturbation(Perturbation):
-    def __new__(
-        cls,
-        resid_acts: Float[torch.Tensor, "... n_ctx d_model"],
-        feature_acts: Float[torch.Tensor, "... n_ctx d_sae"],
-        sae: SAE,
-    ) -> Float[torch.Tensor, "... n_ctx d_model"]:
-        """Toward SAE reconstruction"""
-        return sae.decode(feature_acts) - resid_acts
+    """Toward SAE reconstruction"""
+
+    sae: SAE
+
+    def generate(self, resid_acts):
+        feature_acts = self.sae.encode(resid_acts)
+        return self.sae.decode(feature_acts) - resid_acts
 
 
-class AlongResidActsPerturbation(Perturbation):
-    def __new__(
-        cls, resid_acts: Float[torch.Tensor, "... n_ctx d_model"]
-    ) -> Float[torch.Tensor, "... n_ctx d_model"]:
-        """Scale the residuals"""
+class AmplifyResidActsPerturbation(Perturbation):
+    """Amplify the residual stream"""
+
+    def generate(self, resid_acts):
         return resid_acts
+
+
+amplify_resid_acts_perturbation = AmplifyResidActsPerturbation()
