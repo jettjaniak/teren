012498099ce7df7a112b3df4aba3e77ca2d172a8
Jett Janiak <jettjaniak@gmail.com>
2024-07-17 17:59:27 +0000

    Perturbation.get_pert_by_fid() & FeatureId

--- teren/perturbations.py
+++ teren/perturbations.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import final
+from typing import Iterable, final
 
 import torch
 from jaxtyping import Float
 from sae_lens import SAE
+
+from teren.typing import FeatureId
 
 
 @dataclass(kw_only=True)
@@ -13,10 +15,10 @@ class Perturbation(ABC):
     def __call__(
         self, resid_acts: Float[torch.Tensor, "*batch seq model"]
     ) -> Float[torch.Tensor, "*batch seq model"]:
-        """Ensures that all generate method has correct signature with beartype"""
+        """Ensures that all generate methods have correct signature with beartype"""
         perturb = self.generate(resid_acts)
         assert perturb.device == resid_acts.device
-        # FIXME: we want something like np.shares_memory, this isn't it
+        # TODO: we want something like np.shares_memory, this isn't it
         assert resid_acts.data_ptr() != perturb.data_ptr()
         assert perturb.shape == resid_acts.shape
         assert perturb.isfinite().all()
@@ -26,19 +28,28 @@ class Perturbation(ABC):
     def generate(self, resid_acts: Float[torch.Tensor, "*batch seq model"]):
         raise NotImplementedError
 
+    @abstractmethod
+    @classmethod
+    def get_pert_by_fid(cls, **kwargs) -> dict[FeatureId, "Perturbation"]:
+        raise NotImplementedError
 
-@dataclass
+
+@dataclass(kw_only=True)
 class NaiveRandomPerturbation(Perturbation):
     """Isotropic random"""
 
     def generate(self, resid_acts):
         return torch.randn(resid_acts.shape, device=resid_acts.device)
 
+    @classmethod
+    def get_pert_by_fid(
+        cls, fids: Iterable[FeatureId]
+    ) -> dict[FeatureId, "NaiveRandomPerturbation"]:
+        pert = cls()
+        return {fid: pert for fid in fids}
 
-naive_random_perturbation = NaiveRandomPerturbation()
 
-
-@dataclass
+@dataclass(kw_only=True)
 class TowardSAEReconPerturbation(Perturbation):
     """Toward SAE reconstruction"""
 
@@ -49,8 +60,15 @@ class TowardSAEReconPerturbation(Perturbation):
         feature_acts = self.sae.encode(resid_acts_dev)
         return self.sae.decode(feature_acts).to(resid_acts.device) - resid_acts
 
+    @classmethod
+    def get_pert_by_fid(
+        cls, fids: Iterable[FeatureId], sae: SAE
+    ) -> dict[FeatureId, "TowardSAEReconPerturbation"]:
+        pert = cls(sae=sae)
+        return {fid: pert for fid in fids}
 
-@dataclass
+
+@dataclass(kw_only=True)
 class AmplifyResidActsPerturbation(Perturbation):
     """Amplify the residual stream"""
 
@@ -60,8 +78,15 @@ class AmplifyResidActsPerturbation(Perturbation):
         # FIXME: compute this with running average from the whole dataset
         return resid_acts.clone() - self.resid_mean
 
+    @classmethod
+    def get_pert_by_fid(
+        cls, fids: Iterable[FeatureId], resid_mean: Float[torch.Tensor, "seq model"]
+    ) -> dict[FeatureId, "AmplifyResidActsPerturbation"]:
+        pert = cls(resid_mean=resid_mean)
+        return {fid: pert for fid in fids}
 
-@dataclass
+
+@dataclass(kw_only=True)
 class DampenResidActsPerturbation(AmplifyResidActsPerturbation):
     """Dampen the residual stream"""
 
@@ -69,10 +94,10 @@ class DampenResidActsPerturbation(AmplifyResidActsPerturbation):
         return -super().generate(resid_acts)
 
 
-@dataclass
+@dataclass(kw_only=True)
 class AmplifySEAFeaturePerturbation(Perturbation):
     sae: SAE
-    feature_id: int
+    fid: FeatureId
 
     @property
     def d_sae(self):
@@ -87,8 +112,9 @@ class AmplifySEAFeaturePerturbation(Perturbation):
         W_dec = self.sae.W_dec
         assert W_dec.shape == (self.d_sae, d_model)
 
+        fid_int = self.fid.int
         # (1, d_model)
-        feature_dir = self.sae.W_dec[self.feature_id : self.feature_id + 1]
+        feature_dir = self.sae.W_dec[fid_int : fid_int + 1]
         assert feature_dir.shape == (1, d_model)
 
         # (batch, n_ctx, d_sae)
@@ -96,7 +122,7 @@ class AmplifySEAFeaturePerturbation(Perturbation):
         assert all_feature_acts.shape == (batch, n_ctx, self.d_sae)
 
         # (batch, n_ctx, 1)
-        feature_acts = all_feature_acts[..., self.feature_id : self.feature_id + 1]
+        feature_acts = all_feature_acts[..., fid_int : fid_int + 1]
         assert feature_acts.shape == (batch, n_ctx, 1)
 
         # (batch, n_ctx, d_model)
@@ -105,7 +131,14 @@ class AmplifySEAFeaturePerturbation(Perturbation):
 
         return update.to(resid_acts.device)
 
+    @classmethod
+    def get_pert_by_fid(
+        cls, fids: Iterable[FeatureId], sae: SAE
+    ) -> dict[FeatureId, "AmplifySEAFeaturePerturbation"]:
+        return {fid: cls(sae=sae, fid=fid) for fid in fids}
 
+
+@dataclass(kw_only=True)
 class DampenSEAFeaturePerturbation(AmplifySEAFeaturePerturbation):
     def generate(self, resid_acts):
         return -super().generate(resid_acts)
