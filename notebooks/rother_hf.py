0d31554e7307ca5a31237cc913b206295ca24299
Jett Janiak <jettjaniak@gmail.com>
2024-08-28 18:25:40 +0000

    ...

--- notebooks/rother_hf.py
+++ notebooks/rother_hf.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # %%
+import random
 import sys
 from collections import defaultdict
 
 import torch
+from scipy import stats
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from teren import dir_act_utils as dau
@@ -20,10 +22,10 @@ SEED = 3
 tu.setup_determinism(SEED)
 INFERENCE_BATCH_SIZE = INFERENCE_TOKENS // SEQ_LEN
 print(f"{INFERENCE_BATCH_SIZE=}")
-N_PROMPTS = 10000
+N_PROMPTS = 1000
 # MODEL_NAME, LAYER_FRAC_PERT = "gemma-2-2b", 0
 # MODEL_NAME, LAYER_FRAC_PERT, LAYER_FRAC_READ = sys.argv[1:4]
-MODEL_NAME = "gpt2"
+MODEL_NAME = "phi-2"
 if len(sys.argv) > 1 and not sys.argv[1].startswith("-"):
     MODEL_NAME = sys.argv[1]
 # MODEL_NAME = "olmo-1b"
@@ -81,18 +83,21 @@ else:
     n_layers = cfg.num_hidden_layers
     d_model = cfg.hidden_size
     d_ff = cfg.intermediate_size
-layer_pert = 0
+distance = "mean"
 if len(sys.argv) > 2:
-    layer_read = int(sys.argv[2])
+    distance = sys.argv[2]
+layer_pert = 2
+if len(sys.argv) > 3:
+    layer_read = int(sys.argv[3])
 else:
     layer_read = n_layers - 1
-print(f"{MODEL_NAME=}, {n_layers=}, {d_model=}, {d_ff=}, {layer_pert=}, {layer_read=}")
+print(f"{MODEL_NAME=}, {n_layers=}, {layer_pert=}, {layer_read=}, {distance=}")
 hook_name_pert = MODULE_TEMPLATE.replace("L", str(layer_pert))
 hook_name_read = MODULE_TEMPLATE.replace("L", str(layer_read))
 
 # %%
 input_ids = dau.get_input_ids(
-    chunk=0, seq_len=SEQ_LEN, n_prompts=10_000, tokenizer=tokenizer
+    chunk=0, seq_len=SEQ_LEN, n_prompts=1_000, tokenizer=tokenizer
 ).to(device)
 print(f"{input_ids.shape=}")
 
@@ -167,7 +172,7 @@ import matplotlib.pyplot as plt
 # %%
 N_STEPS = 21
 pert_out = torch.zeros(N_STEPS, N_PROMPTS - 1, d_model)
-mean_dist = torch.zeros(N_STEPS, N_PROMPTS - 1)
+dist = torch.zeros(N_STEPS, N_PROMPTS - 1)
 ls = torch.linspace(1, 0, N_STEPS)
 for i, step in enumerate(ls):
     resid_acts_a = clean_resid_acts[:-1]
@@ -176,7 +181,10 @@ for i, step in enumerate(ls):
     resid_acts_b_last = resid_acts_b[:, -1]
     pert_resid_acts = resid_acts_a.clone()
     pert_resid_acts[:, -1] = step * resid_acts_a_last + (1 - step) * resid_acts_b_last
-    mean_dist[i] = (pert_resid_acts[:, -1] - resid_mean).norm(dim=-1, p=2)
+    if distance == "mean":
+        dist[i] = (pert_resid_acts[:, -1] - resid_mean).norm(dim=-1, p=2)
+    elif distance == "orig":
+        dist[i] = pert_resid_acts[:, -1].norm(dim=-1, p=2)
     pert_run_out = pert_run(pert_resid_acts)
     pert_out[i] = pert_run_out[:, -1]
 print(f"{pert_out.shape=}")
@@ -191,53 +199,67 @@ print(f"{norms.isfinite().all()=}")
 # %%
 norms_max = norms.max(dim=0).values
 norms_mask = norms_max > 0.01
-mean_dist_max_diff = mean_dist.max(dim=0).values - mean_dist.min(dim=0).values
-q90 = torch.quantile(mean_dist_max_diff, 0.95)
-q10 = torch.quantile(mean_dist_max_diff, 0.05)
-print(f"{q90=}")
-mean_dist_mask90 = mean_dist_max_diff > q90
-mean_dist_mask10 = mean_dist_max_diff < q10
 
-mask90 = norms_mask & mean_dist_mask90
-mean_dist_ = mean_dist[:, mask90]
-argmin = mean_dist_.argmin(dim=0)
-print(f"{argmin=}")
-norms_ = norms[:, mask90]
-norms_max_ = norms_max[mask90]
-nnorms = norms_ / norms_max_
-plt.plot(ls.tolist()[::-1], nnorms[:, argmin > 14], color="blue", alpha=0.2)
-plt.plot(ls.tolist()[::-1], nnorms[:, argmin <= 6], color="red", alpha=0.2)
-plt.show()
-# mask10 = norms_mask & mean_dist_mask10
-# title90 = f"{MODEL_NAME}, {n_layers=}, L{layer_pert} to L{layer_read}, 90th percentile"
-# plt.plot(
-#     ls.tolist()[::-1],
-#     norms[:, mask90] / norms_max[mask90],
-# )
-# plt.title(title90)
-# plt.show()
-# plt.plot(
-#     ls.tolist()[::-1],
-#     mean_dist[:, mask90],
-# )
-# plt.title(title90)
-# plt.show()
-# title10 = f"{MODEL_NAME}, {n_layers=}, L{layer_pert} to L{layer_read}, 10th percentile"
-# plt.plot(
-#     ls.tolist()[::-1],
-#     norms[:, mask10] / norms_max[mask10],
-# )
-# plt.title(title10)
-# plt.show()
-# plt.plot(
-#     ls.tolist()[::-1],
-#     mean_dist[:, mask10],
-# )
-# plt.title(title10)
-# plt.show()
-# plt.savefig(f"plots_mean/rother_{MODEL_NAME}_L{layer_pert}_L{layer_read}.png")
+dist_ = dist[:, norms_mask]
+min_loc = dist_.argmin(dim=0)
+has_min_mask = (min_loc > 0) & (min_loc < N_STEPS - 1)
+print(f"{min_loc=}")
+norms_ = norms[:, norms_mask][:, has_min_mask]
+nnorms = norms_ / norms_[-1]
+dist_ = dist_[:, has_min_mask]
+min_loc = min_loc[has_min_mask]
+# %%
+import matplotlib
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+for i in reversed(range(N_STEPS - 1)):
+    cmap = matplotlib.colormaps["jet"]
+    cnum = 1 - i / (N_STEPS - 1)
+    color = cmap(cnum)
+    sel_dist = dist_[:, min_loc == i]
+    sel_norms = nnorms[:, min_loc == i]
+    # take up to 10 random samples
+    sel_dist = sel_dist[:, torch.randperm(sel_dist.shape[1])[:10]]
+    sel_norms = sel_norms[:, torch.randperm(sel_norms.shape[1])[:10]]
+    ax[0].plot(ls.tolist()[::-1], sel_dist, color=color, alpha=0.3)
+    ax[1].plot(ls.tolist()[::-1], sel_norms, color=color, alpha=0.3)
+ax[0].set_title(f"$|{distance}-[\\alpha B+(1-\\alpha)A]|_2$ after L{layer_pert}")
+ax[0].set_xlabel("$\\alpha$")
+
+ax[1].set_title(f"normalized $|A-[\\alpha B+(1-\\alpha)A]|_2$ after L{layer_read}")
+ax[1].set_xlabel("$\\alpha$")
+ax[1].get_ylim()
+ax[1].set_ylim(0, 1.4)
+
+fig.suptitle(f"{MODEL_NAME}, {n_layers=}, L{layer_pert} to L{layer_read}")
+fig.show()
+
+fig.savefig(
+    f"plots_dist/rother_{MODEL_NAME}_L{layer_pert}_L{layer_read}_{distance}.png"
+)
+
 
 # %%
+def find_first_above_threshold(tensor, jump_thresh):
+    mask = tensor > jump_thresh
+    return mask.to(torch.int).argmax(dim=0)
 
 
+jump_threshold = 0.7
+jump_point = find_first_above_threshold(nnorms, jump_threshold)
+random_idx = random.sample(range(nnorms.shape[1]), 10)
+default_cmap = plt.get_cmap("tab10")
+for j, i in enumerate(random_idx):
+    color = default_cmap(j)
+    plt.plot(nnorms[:, i], color=color)
+    jp = jump_point[i]
+    plt.axvline(jp, color=color, linestyle="--")
+
+
+# %%
+plt.scatter(jump_point, min_loc, alpha=0.05)
+plt.xlabel("$argmin_{\\alpha} |mean-[\\alpha B+(1-\\alpha)A]|_2$")
+plt.ylabel(f"min $\\alpha$ s.t. normalized dist after L{layer_read} > {jump_threshold}")
+plt.show()
+stats.pearsonr(jump_point, min_loc)
 # %%
